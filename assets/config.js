4784bd423b3777e4efe36aca648bbd1ec070bde0
PAYHUB local apply <programmer119@users.noreply.github.com>
2026-08-22 23:58:12 +0900

    Deploy PAYHUB WEB v2026.08.22.2357

--- assets/config.js
+++ assets/config.js
@@ -1,1 +1,1 @@
-window.PAYHUB_CONFIG = { apiBase: 'https://api-payhub.suaveforge.com', buildId: 'v2026.08.22.2344', sourceCommit: '11baf3d730775eb85feab57fe083951766a1499a' };
+window.PAYHUB_CONFIG = { apiBase: 'https://api-payhub.suaveforge.com', buildId: 'v2026.08.22.2357', sourceCommit: '11baf3d730775eb85feab57fe083951766a1499a' };
